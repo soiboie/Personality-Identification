8e425cbba582487318b23d481fb54bf0b07f9cec
soiboie <126751552+soiboie@users.noreply.github.com>
2025-01-04 13:08:01 +0530

    Create side.js

---
+++ side.js
@@ -0,0 +1,50 @@
+let quizData = [
+  
+    {
+      question: "You prefer:",
+      options: ["Large Social Gatherings", "One-on-one interactions", "Small groups of close friends", "Being alone"],
+      extrovertScore: 2,
+      introvertScore: 1,
+      extrovertScore: 1,
+      introvertScore: 2
+    },
+    {
+      question: "When you have free time, you prefer to:",
+      options: ["Go out and socialize", "Stay in and recharge", "Balance staying in and going out", "Spend time alone or with a small group"],
+      extrovertScore: 2,
+      introvertScore: 2,
+      extrovertScore: 1,
+      introvertScore: 1
+    },
+    {
+      question: "You usually describe yourself as:",
+      options: ["Outgoing and sociable", "Quiet and introspective", "Ambivert (balanced between introversion and extroversion)", "Sometimes outgoing, sometimes quiet"],
+      extrovertScore: 2,
+      introvertScore: 2,
+      extrovertScore: 1,
+      introvertScore: 1
+    },
+    {
+      question: "You feel energized after:",
+      options: ["A lively party", "A quiet evening at home", "Both, depending on the situation", "Neither, I recharge in my own way"],
+      extrovertScore: 2,
+      introvertScore: 2,
+      extrovertScore: 1,
+      introvertScore: 1
+    },
+    {
+      question: "You prefer to work:",
+      options: ["In teams", "Alone", "In a balanced setting", "Collaboratively but with some alone time"],
+      extrovertScore: 2,
+      introvertScore: 2,
+      extrovertScore: 1,
+      introvertScore: 1
+    },
+    {
+      question: "To prepare for a night out...",
+      options: ["I get all my friends together to plan where we'll dance the night away.", "Prepare ahead of time so I'm not rushed", "Decide last minute, I like the excitement", "Balance planning and spontaneity"],
+      extrovertScore: 2,
+      introvertScore: 2,
+      extrovertScore: 1,
+      introvertScore: 1
+    },
